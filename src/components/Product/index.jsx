2bdf44084cdc554404c99ea7912f916864855a6b
Silva <luther.n.silva@accenture.com>
2022-04-29 11:05:37 -0300

    feat: component Product (card for products)

---
+++ src/components/Product/index.jsx
@@ -0,0 +1,21 @@
+import './styles.css';
+
+const Product = ({product,handleClick }) => {
+
+    return (
+      <div className='card__container'>
+        <div className='card__header'>
+          <img className='card__img' alt={product.name} src={product.img}/>
+        </div>      
+        <div className='card__body'>
+          <h3 className='card__name' >{product.name}</h3>
+          <p className='card__category'>{product.description}</p>
+          <span className='card__price'>R$ {product.price}</span>
+          <span className='card__price__discount'>R$ {product.discount}</span>
+          <button onClick={() => handleClick(product.code)}>Adicionar</button>
+          </div>       
+      </div>
+  );
+};
+
+export default Product;
